0b6b0683a5e3f878c33e49b850e356d0e3abb2be
datitisev <dsevilla192@icloud.com>
2018-12-02 21:12:41 +0000

    Apply fixes from StyleCI

--- src/Controllers/MarkAsSpammerController.php
+++ src/Controllers/MarkAsSpammerController.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of fof/spamblock.
  *
- * Copyright (c) 2018 FriendsOfFlarum
+ * Copyright (c) 2018 FriendsOfFlarum.
  *
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
