8599bd9acec4de6e92ce7e1a73335582a4d29792
Ian Morland <ian@morland.me>
2021-05-26 19:43:31 +0100

    Flarum 1.0

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,10 +1,12 @@
 import app from 'flarum/common/app';
 
 app.initializers.add('fof-spamblock', () => {
-    app.extensionData.for('fof-spamblock')
-        .registerPermission({
+    app.extensionData.for('fof-spamblock').registerPermission(
+        {
             icon: 'fas fa-pastafarianism',
             label: app.translator.trans('fof-spamblock.admin.permissions.spamblock_users_label'),
             permission: 'user.spamblock',
-        }, 'moderate');
+        },
+        'moderate'
+    );
 });
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,6 +1,6 @@
 import { extend } from 'flarum/common/extend';
 import Button from 'flarum/common/components/Button';
-import UserControls from 'flarum/common/utils/UserControls';
+import UserControls from 'flarum/forum/utils/UserControls';
 
 import User from 'flarum/common/models/User';
 import Model from 'flarum/common/Model';
@@ -8,21 +8,24 @@ import Model from 'flarum/common/Model';
 app.initializers.add('fof/spamblock', () => {
     User.prototype.canSpamblock = Model.attribute('canSpamblock');
 
-    extend(UserControls, 'moderationControls', function(items, user) {
+    extend(UserControls, 'moderationControls', function (items, user) {
         if (user.canSpamblock()) {
             items.add(
                 'spammer',
-                Button.component({
-                    icon: 'fas fa-pastafarianism',
-                    onclick: () => {
-                        if (!confirm(app.translator.trans('fof-spamblock.forum.user_controls.spammer_confirmation'))) return;
+                Button.component(
+                    {
+                        icon: 'fas fa-pastafarianism',
+                        onclick: () => {
+                            if (!confirm(app.translator.trans('fof-spamblock.forum.user_controls.spammer_confirmation'))) return;
 
-                        app.request({
-                            url: `${app.forum.attribute('apiUrl')}/users/${user.id()}/spamblock`,
-                            method: 'POST',
-                        }).then(() => window.location.reload());
+                            app.request({
+                                url: `${app.forum.attribute('apiUrl')}/users/${user.id()}/spamblock`,
+                                method: 'POST',
+                            }).then(() => window.location.reload());
+                        },
                     },
-                }, app.translator.trans('fof-spamblock.forum.user_controls.spammer_button'))
+                    app.translator.trans('fof-spamblock.forum.user_controls.spammer_button')
+                )
             );
         }
     });
